bbdef684629c7fdb960ed861c7643da1c72801e7
Dmytro Sagaidachnyi <lafuma4me@gmail.com>
2021-08-01 00:58:55 +0300

    netlify

--- src/App.js
+++ src/App.js
@@ -1,50 +1,34 @@
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import routes from './routes';
+import AppBar from './components/AppBar/AppBar';
+import Spinner from './components/Spinner/Spinner';
 
-import React from "react";
-import { Router, Switch, Route, Link } from "react-router-dom";
+const HomePage = lazy(() =>
+  import('./pages/HomePage.js' /* webpackChunkName: "home-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './pages/MovieDetailsPage.js' /* webpackChunkName: "home-details-page" */
+  ),
+);
+const MoviesPage = lazy(() =>
+  import('./pages/MoviesPage.js' /* webpackChunkName: "movies-page" */),
+);
 
-https://api.themoviedb.org/3/movie/550?api_key=d84d15bdfdec192b10fc2c60fd5ff048
+const App = () => (
+  <>
+    <AppBar />
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/" component = {HomePage} />
-          <Route path='/movies' component = {MoviesPage} />
-          <Route path='/movies/:movieId' component = {MovieDetailsPage} />
-          <Route  />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+    <Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route exact path={routes.home} component={HomePage} />
+        <Route path={routes.movieDetails} component={MovieDetailsPage} />
+        <Route path={routes.movies} component={MoviesPage} />
+        <Redirect to={routes.home} />
+      </Switch>
+    </Suspense>
+  </>
+);
 
 export default App;
